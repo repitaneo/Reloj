217b144cd2718dbc9ee0beecb174add25351d6be
Marcos Fdez <marcosd@educastur.org>
2018-11-29 08:49:40 +0100

    Todo acabado. Con comentarios incluidos

--- src/tictac/Reloj.java
+++ src/tictac/Reloj.java
@@ -7,8 +7,21 @@ public class Reloj {
 	private int hora = 0;
 	private int minutos = 0;
 	private int segundos = 0;
+	private boolean modoCronometro = false;
 	
 	
+	/*
+	 * 
+	 * ESTOS SON LOS GETTERS Y SETTERS
+	 */
+	public boolean isModoCronometro() {
+		return modoCronometro;
+	}
+
+	public void setModoCronometro(boolean modoCronometro) {
+		this.modoCronometro = modoCronometro;
+	}
+
 	public int getHora() {
 		return hora;
 	}
@@ -24,27 +37,44 @@ public class Reloj {
 	
 	
 	public void setHora(int hora) {
-		if((hora>=0)&(hora<=23)) {
+
+		hora = 0;
+		// permito cambiar valores si estoy en modo 
+		// reloj y se cumplen los rangos
+		if((modoCronometro==false)&(hora>=0)&(hora<=23)) {
 			this.hora = hora;
 		}
 	}
 	
 	
 	public void setMinutos(int minutos) {
-		if((minutos>=0)&(minutos<=59)) {
+		
+		minutos = 0;
+		// permito cambiar valores si estoy en modo 
+		// reloj y se cumplen los rangos
+		if((modoCronometro==false)&(minutos>=0)&(minutos<=59)) {
 			this.minutos = minutos;
 		}
 	}
 	
 	public void setSegundos(int nuevosSegundos) {
-		if((nuevosSegundos>=0)&(nuevosSegundos<=59)) {
+		
+		segundos = 0;
+		// permito cambiar valores si estoy en modo 
+		// reloj y se cumplen los rangos
+		if((modoCronometro==false)&(nuevosSegundos>=0)&(nuevosSegundos<=59)) {
 			segundos = nuevosSegundos;
 		}
 	}
 
-	
-	
-	
+
+	/*
+	 ******************************** 
+	 ********************************
+	 *
+	 * OPERACIONES DE AÑADIR SEGUNDOS AL RELOJ
+	 * 
+	 */
 	public void addSegundos() {
 		
 		segundos++;
@@ -59,9 +89,13 @@ public class Reloj {
 				minutos=0;
 				hora++;
 				
-				// arreglo las horas si son 24
-				if(hora==24) {
-					hora=0;
+				// en modo reloj permito el arreglo
+				//if(modoCronometro==false) {
+				if(!modoCronometro) {
+					// arreglo las horas si son 24
+					if(hora==24) {
+						hora=0;
+					}
 				}
 			}
 		}
@@ -70,23 +104,35 @@ public class Reloj {
 	
 	
 
+	/*
+	 ******************************** 
+	 ********************************
+	 *
+	 * OPERACIONES DE RESTAR SEGUNDOS AL RELOJ
+	 * 
+	 */
 	public void subSegundos() {
 		
-		segundos--;
+		// en modo coronometro esta operación 
+		// no tiene sentido
+		//if(modoCronometro==false) { 
+		if(!modoCronometro) { 
 		
-		// arreglo los segundos si son -1
-		if(segundos==-1) {
-			segundos = 59;
-			minutos--;
-			
-			// arreglo los minutos si son -1
-			if(minutos==-1) {
-				minutos=59;
-				hora--;
+			segundos--;
+			// arreglo los segundos si son -1
+			if(segundos==-1) {
+				segundos = 59;
+				minutos--;
 				
-				// arreglo las horas si son -1
-				if(hora==-1) {
-					hora=23;
+				// arreglo los minutos si son -1
+				if(minutos==-1) {
+					minutos=59;
+					hora--;
+					
+					// arreglo las horas si son -1
+					if(hora==-1) {
+						hora=23;
+					}
 				}
 			}
 		}
@@ -96,56 +142,48 @@ public class Reloj {
 	
 	
 	
+	/*
+	 ******************************** 
+	 ********************************
+	 *
+	 * OPERACION DE SUMAR RELOJES
+	 * 
+	 */
 	public Reloj sumarRelojes(Reloj sumando) {
 
+		// sumamos los diferentes parametros
+		// de los relojes
 		int horas = this.hora + sumando.hora;
 		int minutos = this.minutos + sumando.minutos;
 		int segundos = this.segundos + sumando.segundos;
 		
+		// arreglamos los errores que haya
 		if(segundos>=60) {
+			// si nos pasamos en 1 minutos, 
+			// descontamos 60s y sumamos el minuto
 			segundos-=60;
 			minutos++;
 		}
 		if(minutos>=60) {
+			// si nos pasamos en 1 hora, 
+			// descontamos 60min y sumamos la hora
 			minutos-=60;
 			horas++;
 		}
-		
+
+		// creo un nuevo reloj y establezco
+		// los valores que he calculado
 		Reloj devolver = new Reloj();
 		devolver.hora = horas;
 		devolver.minutos = minutos;
 		devolver.segundos = segundos;
-		
+
+		// devuelvo el objeto creado
 		return devolver;
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public String toString()  {
 		
 		return hora+":"+minutos+":"+segundos;
